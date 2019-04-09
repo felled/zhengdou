53ae62c4f4e9236e3fc81b6d8f2eccd18a06bf66
felled <931430246@qq.com>
2019-04-09 23:51:59 +0800

    55

--- js/projrct.js
+++ js/projrct.js
@@ -1,17 +1,17 @@
 $(function () {
     var offsetWid = document.documentElement.clientWidth;
 
-      $(".tab-list span").hover(function () {
-          var e =$(this).attr("name");
-          $(".poj-characteristic > div").addClass("hidden");
-          $(".poj-characteristic").find("."+e+"-char").removeClass("hidden");
-          $(".tab-list span").removeClass("active");
-          $(this).addClass("active");
-      })
-      /*pc-code*/
-      $(".code-img").hover(function () {
-          $(".hover-code").toggleClass("hover");
-      });
+    $(".tab-list span").hover(function () {
+        var e =$(this).attr("name");
+        $(".poj-characteristic > div").addClass("hidden");
+        $(".poj-characteristic").find("."+e+"-char").removeClass("hidden");
+        $(".tab-list span").removeClass("active");
+        $(this).addClass("active");
+    })
+    /*pc-code*/
+    $(".code-img").hover(function () {
+        $(".hover-code").toggleClass("hover");
+    });
     if(offsetWid < 1024){
         /*mb-code*/
         $(".code-img,.code-a").click(function () {
--- js/public.js
+++ js/public.js
@@ -10,25 +10,25 @@ $(function () {
     });
     /*mb-head*/
 
-        $(".page-open").click(function () {
-            var e = $(this).attr("name");
-            $(".fir").removeClass("show-page");
-            $("."+ e +"").addClass("show-page");
-        });
+    $(".page-open").click(function () {
+        var e = $(this).attr("name");
+        $(".fir").removeClass("show-page");
+        $("."+ e +"").addClass("show-page");
+    });
 
-        $(".mb-muen").click(function () {
-            $(".fir").addClass("show-page");
-            $(".black").css("display","block");
-        });
+    $(".mb-muen").click(function () {
+        $(".fir").addClass("show-page");
+        $(".black").css("display","block");
+    });
 
-        $(".fir .black").click(function () {
-            $(this).parents(".mb-page").removeClass("show-page");
-            $(this).css("display","none");
-        });
-        $(".other-page .black").click(function () {
-            $(this).parents(".mb-page").removeClass("show-page");
-            $(".fir").addClass("show-page");
-        });
+    $(".fir .black").click(function () {
+        $(this).parents(".mb-page").removeClass("show-page");
+        $(this).css("display","none");
+    });
+    $(".other-page .black").click(function () {
+        $(this).parents(".mb-page").removeClass("show-page");
+        $(".fir").addClass("show-page");
+    });
 
     /*pojrct*/
     $(".poj-info").click(function (event) {
